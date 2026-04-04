1d09687809e62237dade72cc55330bb952cf7e41
srigokul001 <sri08032008@gmail.com>
2026-04-04 18:37:14 +0530

    cors fix for github pages

--- myspotify-backend/server.js
+++ myspotify-backend/server.js
@@ -10,7 +10,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: '*'
+  origin: ['https://srigokul001.github.io', 'http://127.0.0.1:5500', 'http://localhost:5500'],
+  methods: ['GET', 'POST'],
+  credentials: true
 }));
 app.use(express.json());
 
